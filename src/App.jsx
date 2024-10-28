accac3ca01d50cfafbc7e7276f6ffd30f4b566c9
Fanicuki <faygamer321@gmail.com>
2024-10-28 10:28:01 -0300

    branch de pato

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// PÁGINA PRINCIPAL
+
+import Sidebar from './lib/sidebar.jsx'
+import './lib/sidebar.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className='main-container'>
+      <Sidebar />
+      <div className="container">
+          <p>hola chiquito</p>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/lib/sidebar.jsx
@@ -0,0 +1,20 @@
+// BARRA LATERAL IZQUIERDA
+
+function Sidebar() {
+
+    return (
+        <>
+        <div class="base-navbar">
+            <div class="windows">
+                <div class="windows-btn">
+                    <button><h3>Pañol</h3></button>
+                    <button><h3>Académicas</h3></button>
+                </div>
+                <p>Hola</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Sidebar
